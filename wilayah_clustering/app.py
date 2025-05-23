a0f2547cefbd82dd9410aa5a8e1948eca1324e72
HilmiNurpadilah <158576356+HilmiNurpadilah@users.noreply.github.com>
2025-05-23 14:23:06 +0700

    Update app.py

--- wilayah_clustering/app.py
+++ wilayah_clustering/app.py
@@ -140,7 +140,4 @@ def index():
     )
 
     if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port)
-
+    app.run(host='0.0.0.0', port=8000)
